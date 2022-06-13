ec7ca5f6082d72e7bb2c9da0ae9440256ffa6a11
alexisben <alexiben7@gmail.com>
2022-06-13 12:14:03 +0200

    Simplify

--- experience/SceneView/SceneView.js
+++ experience/SceneView/SceneView.js
@@ -1,33 +1,19 @@
 import { Mesh, SphereBufferGeometry, MeshStandardMaterial } from 'three';
 import SceneBase from './Scene/SceneBase';
-import ASSETS from './assets';
 
 export default class SceneView extends SceneBase {
     isReady = false;
     init () {
         super.init();
-        this.load();
 
         // HELPERS
         this.setControls();
         this.setHelpers();
-    }
 
-    preload () {
-        if (ASSETS.length > 0) {
-            this.load();
-        } else {
-            this.setup();
-        }
-    }
-
-    load () {
-        // WHEN ASSETS LOADED
         this.setup();
     }
 
     setup () {
-        // AFTER ASSETS LOADED
         this.addTestBox();
         this.isReady = true;
     }
@@ -36,9 +22,7 @@ export default class SceneView extends SceneBase {
         const sphere = new Mesh(
             new SphereBufferGeometry(1, 32, 32),
             new MeshStandardMaterial({
-                color: 0xDDDDDD,
-                metalness: 1,
-                roughness: 0.3
+                color: 0xDDDD00
             })
         );
 
--- experience/SceneView/assets.js
+++
@@ -1,7 +0,0 @@
-const base = '/assets/images';
-
-const ASSETS = {
-    studio: `${base}/textures/studio.hdr`,
-};
-
-export default ASSETS;
